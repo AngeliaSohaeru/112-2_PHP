96dd53e4e2d0ad859b5dddd1ef6feb88e94a7ebc
AngeliaSohaeru <125714889+AngeliaSohaeru@users.noreply.github.com>
2024-05-30 15:31:00 +0800

    add.php

---
+++ Week12/add.php
@@ -0,0 +1,8 @@
+<meta charset = 'utf-8'>
+
+<form action = "adddb.php" method = "post">
+<input type = "text" name = "Name"><br/>
+<input type = "text" name = "Id"><br/>
+<input type = "text" name = "Department"><br/>
+<input type = "submit">
+</form>
